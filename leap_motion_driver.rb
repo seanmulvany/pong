3e4306be7738333fdc442b2aed2244ca16725380
Sean M <sean.mulvany@gmail.com>
2015-02-15 20:01:49 +0000

    One Player Pong & LeapMotion

---
+++ leap_motion_driver.rb
@@ -0,0 +1,57 @@
+
+require "artoo"
+
+
+class LeapPong < Artoo::MainRobot
+
+
+  connection :leapmotion, :adaptor => :leapmotion, :port => '127.0.0.1:6437'
+  device :leapmotion, :driver => :leapmotion
+
+
+
+  work do
+    on leapmotion, :open => :on_open
+    on leapmotion, :frame => :on_frame
+    on leapmotion, :close => :on_close
+
+
+  end
+
+
+  def on_open(*args)
+    puts args
+  end
+
+  def on_frame(*args)
+
+    frame = args[1]
+    frame
+
+    frame.hands.each do |element|
+      p element.stabilizedPalmPosition[1]
+      case when element
+             read = File.open('console.out', 'w')
+             read << element.stabilizedPalmPosition[1]
+             read.close
+
+      end
+
+    end
+
+  end
+
+
+  def on_close(*args)
+    puts args
+  end
+
+
+
+
+end
+
+LeapPong.work!
+
+
+
--- test.rb
+++ test.rb
@@ -1,32 +1,57 @@
-require "gosu"
-include Gosu
+
+require "artoo"
 
 
-class Game < Window
-  WIDTH = 736
-  HEIGHT = 414
+class LeapPong < Artoo::MainRobot
 
-  def initialize
- super HEIGHT, WIDTH, false
-    self.caption = "TEST"
-    @points = []
-  end
 
-  def update
-    @points << [mouse_x, mouse_y]
-  end
+connection :leapmotion, :adaptor => :leapmotion, :port => '127.0.0.1:6437'
+device :leapmotion, :driver => :leapmotion
 
-  def draw
-    return if @points.empty?
-    @points.inject(@points[0]) do |last|
-      draw_line last[0], last[1], Color::GREEN
 
-      point
+
+work do
+  on leapmotion, :open => :on_open
+  on leapmotion, :frame => :on_frame
+  on leapmotion, :close => :on_close
+
+
+end
+
+
+def on_open(*args)
+  puts args
+end
+
+def on_frame(*args)
+
+array = []
+frame = args[1]
+   frame
+
+  frame.hands.each do |element|
+  p element.stabilizedPalmPosition[1]
+    case when element
+           read = File.open('console.out', 'w')
+            read << element.stabilizedPalmPosition[1]
+           read.close
+
     end
-  end
+
+    end
+
+end
+
+
+def on_close(*args)
+  puts args
+end
 
 
 
 end
 
-Game.new.show
+LeapPong.work!
+
+
+
